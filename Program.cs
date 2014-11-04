62191bd7a5c56f4535e4b99082a80d980f22df7b
rovepe17 <rociounipamplona@hotmail.com>
2014-11-04 14:15:27 -0500

    Appcuadrado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appcuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado, perimetro;
+            Console.WriteLine("programa que calcule perimetro de cuadrado");
+            Console.WriteLine("digite lado de un cuadrado");
+            lado = int.Parse(Console.ReadLine());
+            perimetro = lado * 4;
+            Console.WriteLine("el perimetro es:" + perimetro);
+            Console.ReadKey();
+
+
+        }
+    }
+}
